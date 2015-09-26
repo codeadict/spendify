a881d21d9d71573761528400dd35a7d37b6d9924
Dairon Medina <dairon.medina@gmail.com>
2015-09-25 23:11:03 -0500

    added initial project structure with grunt, bower, sass

---
+++ README.md
@@ -0,0 +1,3 @@
+### Spendify
+
+This is a simple expense tracking app built while learning **Node.js** stack and other fun Javascript weapons.
---
+++ models/goal.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var GoalSchema = new Schema({
+
+});
+
+module.exports = mongoose.model('Goal', GoalSchema);
